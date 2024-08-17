7842a429b75974963048844a19bce3dba15ebfa7
Adwaith-anand-sr <sreeadwa@gmail.com>
2024-08-17 08:10:09 +0530

    added chatModel

--- models/chats.js
+++ models/chats.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 const chatSchema = mongoose.Schema({
-	sender: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
-	receiver: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
-	message: String,
-	timestamp: { type: Date, default: Date.now },
-	isrecieved: { type: Boolean, default: false },
-	isreaded: { type: Boolean, default: false },
+   chatId: String,
+	participants: [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "user"
+		}],
+	messages: [{
+	   sender: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+	   reciever: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+	   message: String,
+	   isrecieved: { type: Boolean, default: false },
+	   isreaded: { type: Boolean, default: false },
+	   timestamp: { type: Date, default: Date.now },
+	}],
+	
 });
 
 module.exports = mongoose.model("chat", chatSchema);
